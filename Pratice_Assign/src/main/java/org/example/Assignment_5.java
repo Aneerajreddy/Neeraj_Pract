575a92678161a69fa143397eefeb7d16f64079d1
Neeraj <neerajreddy14@gmail.com>
2025-04-20 11:20:07 -0400

    assignment-5 update

--- Pratice_Assign/src/main/java/org/example/Assignment_5.java
+++ Pratice_Assign/src/main/java/org/example/Assignment_5.java
@@ -1,4 +1,25 @@
 package org.example;
 
 public class Assignment_5 {
+
+// JAR and WAR? What different build tools are available?
+
+// What is Encapsulation, Inheritance, polymorphism, interface, abstract classes?
+
+// Create setters and getters for different class variables for class Employee?
+
+// Read what are getters & setters, what are access modifiers, what is difference between public, private, protected and default?
+
+// Understand the access modifier usage when applied at class level, method level and variable level?
+// Create setters and getters for different class variables for class Customer, with customerID, customerName, customerAddress, customerZipcode, customerName and create 5 different objects using setters & getters. Write these objects to a file ?
+// Convert the customer objects into table and write it to a file
+// Convert the customer objects into JSON format and write it to a file
+// Write a java code to insert 5 elements into array with for loop
+// Write a java program to retrieve 5 elements in an array
+// Write a java code to retrieve an element based on the index
+
+    public static void main (String [] args){
+
+    }
+
 }
