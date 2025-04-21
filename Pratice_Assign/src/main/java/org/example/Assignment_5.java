929bdf59c1ac6b7ace1cdf1c76cc8221a151d191
Neeraj <neerajreddy14@gmail.com>
2025-04-20 23:05:14 -0400

    assignment-5 update

--- Pratice_Assign/src/main/java/org/example/Assignment_5.java
+++ Pratice_Assign/src/main/java/org/example/Assignment_5.java
@@ -4,9 +4,28 @@ public class Assignment_5 {
 
 // JAR and WAR? What different build tools are available?
 
+  //  Jar(Java ARchive) is a compressed file that contains all different packages
+  //A WAR (Web Application Archive) file is used to package web applications,
+    // including servlets, JSPs, HTML files, and other web components,
+    // and is deployed on a servlet container like Apache Tomcat.
+
 // What is Encapsulation, Inheritance, polymorphism, interface, abstract classes?
+//Encapsulation is the concept of bundling data and methods that operate on that data within a single unit or class, and restricting access to some of the object’s components.
+// Inheritance is the mechanism by which one class can acquire the properties and behaviors of another class, promoting code reusability.
+// Polymorphism allows objects to be treated as instances of their parent class rather than their actual class, enabling a single interface to represent different underlying forms.
+// An interface is a reference type in Java that contains only abstract methods and constants, used to achieve full abstraction and multiple inheritance.
+// An abstract class is a class that cannot be instantiated on its own and may contain abstract methods (without implementation) as well as concrete methods, serving as a base for other classes.
 
 // Create setters and getters for different class variables for class Employee?
+
+    public void Employees(){
+
+        String First_name ;
+        String Last_name ;
+        String Email;
+        int Salary;
+
+    }
 
 // Read what are getters & setters, what are access modifiers, what is difference between public, private, protected and default?
 
